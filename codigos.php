551d15dbbd2bb63aa4659ab93c5f91a1e33bb667
Aline Cardoso Alexandrte <32268771+alinealexandre@users.noreply.github.com>
2019-08-15 11:14:55 -0300

    Add files via upload

---
+++ codigos.php
@@ -0,0 +1,46 @@
+<?php
+
+create_function(ipint, masc)
+
+if ($masc = "/24") {
+	echo 
+}elseif ($masc = "/25") {
+	# code...
+}elseif ($masc = "/26") {
+	# code...
+}elseif ($masc = "/27") {
+	# code...
+}elseif ($masc = "/28") {
+	# code...
+}elseif ($masc = "/29") {
+	# code...
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+	<style>
+		body {
+		background: #cef !important;
+		}
+	</style>
 	<title> Calculadora IP </title>
 	 <script type="text/javascript" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
  	 <link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
@@ -9,13 +15,7 @@
 	<nav>
 		<div class="pusher cor2">
 
-		    <div class="ui container ">
-		      <div class="ui large secondary pointing menu ">
 
-		        <a  href="index.php" class="active item blue">Home</a>
-		        <a  href="#" class="item">BlaBla</a>
-		        
-		    </div>
 		<div class="barra_menu">
 			<img id="menu" src="logo_calculadora.png">
 		</div>
@@ -30,17 +30,93 @@
 		      <form class="ui form">
 		        <br>
 		        <div class="field">
-				    <label>IP</label>
-				    <input type="text" name="ip" placeholder="ex: 192.168.1.0">
+				    <h3 id="letra1">Informe o IP e a máscara:</h3>
+				    <input  class="input" type="text" name="ip_completo" placeholder="ex: 192.168.0.1"><h4 id="letra2">.</h4>
+
 				  </div>
 				  <div class="field">
-				    <label>Máscara</label>
-				    <input type="text" name="mascara" placeholder="ex: 24 ou 255.255.255.0">
+				    <input type="text" class="input" name="mascara" placeholder="ex: 24">
 				  </div>
-				  <button class="ui button"  id="botao" type="submit">Calcular</button>
+				  <button class="ui button botao"  id="botao" type="submit">Calcular</button>
 		      </form>
 
 		    </div>
+		</div>
+		
+		<div class="retangulo">
+			<div class="container" id="isso">
+				
+			<table class="ui celled table tabela">
+			  <thead>
+			    <tr><th>IP</th>
+			    <th>IPs por rede</th>
+			    <th>Sub-redes</th>
+			    <th>Classe</th>
+			    <th>Público ou Privado</th>
+			    <th>Máscara</th>
+			    <th>Redes</th>
+			  </tr></thead>
+			  <tbody>
+			    <tr>
+			    <td data-label="ip" >122.144.122.100</td>
+			      <td data-label="ip_por_rede">64</td>
+			      <td data-label="sub_rede">4</td>
+			      <td data-label="Classe">A</td>
+			      <td data-label="pub_ou_priv">Público</td>
+			      <td data-label="mascara">255.255.255.192</td>
+			      <td data-label="redes">1</td>
+			    </tr>
+			   </tbody>
+			</table>
+
+			<table class="ui celled table tabela">
+			    <thead>
+				    <tr><th>sub-rede</th>
+				    <th>Endereço de rede</th>
+				    <th>Broadcast</th>
+				    <th>Primeiro IP</th>
+				    <th>último IP</th>
+			    </tr></thead>
+
+
+			    <tr >
+			      <td data-label="qual_sub_rede">0</td>
+			      <td data-label="end_rede">122.144.122.0</td>
+			      <td data-label="broadcast">122.144.122.127</td>
+			      <td data-label="primeiro_IP">122.144.122.65</td>
+			      <td data-label="ultimo_IP">122.144.122.126</td>
+			    </tr>
+			    <tr>
+			      <td data-label="qual_sub_rede">1</td>
+			      <td data-label="end_rede">122.144.122.0</td>
+			      <td data-label="broadcast">122.144.122.127</td>
+			      <td data-label="primeiro_IP">122.144.122.65</td>
+			      <td data-label="ultimo_IP">122.144.122.126</td>
+			    </tr>
+			    <tr>
+			      <td data-label="qual_sub_rede">2</td>
+			      <td data-label="end_rede">122.144.122.0</td>
+			      <td data-label="broadcast">122.144.122.127</td>
+			      <td data-label="primeiro_IP">122.144.122.65</td>
+			      <td data-label="ultimo_IP">122.144.122.126</td>
+			    </tr>
+			    <tr>
+			      <td data-label="qual_sub_rede">3</td>
+			      <td data-label="end_rede">122.144.122.0</td>
+			      <td data-label="broadcast">122.144.122.127</td>
+			      <td data-label="primeiro_IP">122.144.122.65</td>
+			      <td data-label="ultimo_IP">122.144.122.126</td>
+			    </tr>
+			  </tbody>
+			</table>
+
+
+
+			
+			</div>
+		
+		
+		
 		</div>
 
 </body>
